49cc32d1773eaf2a871776558f8b069c424db3bf
leo-ronan <leoronan15@gmail.com>
2021-01-27 23:14:08 -0500

    added switch statement and all cases + function names to be used per case

--- server1.js
+++ server1.js
@@ -1,6 +1,7 @@
 //REQUIRE DEPENDENCIES
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+const { registerPrompt } = require("inquirer");
 
 //SET DATABASE TO BE USED, CREDENTIALS NEEDED, AND HOST/PORT
 const dbConnection = mysql.createConnection({
@@ -31,7 +32,74 @@ function queryUser() {
             "Remove employee",
             "Remove job role",
             "Remove department",
-            "List "
+            "Display all employees",
+            "Display employees by department",
+            "Display employees by job role",
+            "Display employees by assigned manager",
+            "Change an employee's name",
+            "Change an employee's job role",
+            "Change an employee's department",
+            "End program"
         ]
+    })
+    //HANDLE USER INPUT, RUN FUNCTION AFFILIATED WITH EACH OPTION
+    .then(function(res) { 
+        switch(res.home) {
+            case "Add new employee":
+                addEmployee();
+            break;
+
+            case "Add new job role":
+                addRole();
+            break;
+
+            case "Add new department":
+                addDepartment();
+            break;
+
+            case "Remove employee":
+                rmEmployee();
+            break;
+
+            case "Remove job role":
+                rmRole();
+            break;
+            
+            case "Remove department":
+                rmDepartment();
+            break;
+
+            case "Display all employees":
+                allEmployees();
+            break;
+
+            case "Display employees by department":
+                departmentEmployees();
+            break;
+
+            case "Display employees by job role":
+                roleEmployees();
+            break;
+
+            case "Display employees by assigned manager":
+                managerEmployees();
+            break;
+
+            case "Change an employee's name":
+                setEmployeeName();
+            break;
+
+            case "Change an employee's job role":
+                setEmployeeJob();
+            break;
+            
+            case "Change an employee's department":
+                setEmployeeDepartment();
+            break;
+
+            case "End program":
+                dbConnection.end();
+            break;
+        }
     })
 }
