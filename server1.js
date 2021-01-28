56e29d3b2506b0b9c499003ebb8433fa3bc3741b
leo-ronan <leoronan15@gmail.com>
2021-01-27 23:02:00 -0500

    Initial queryUser function created. Only contains initial inquirer choices now.

--- server1.js
+++ server1.js
@@ -1,6 +1,5 @@
 //REQUIRE DEPENDENCIES
 const mysql = require("mysql");
-const consoleTable = require("console.table");
 const inquirer = require("inquirer");
 
 //SET DATABASE TO BE USED, CREDENTIALS NEEDED, AND HOST/PORT
@@ -19,5 +18,20 @@ dbConnection.connect(function(err) {
 });
 
 function queryUser() {
-    
+    console.log("DB connected, queryUser() triggered.")
+    //MAIN INQUIRER PROMPT CONTAINING ALL DATABASE-ALTERING OPTIONS THE USER MAY SELECT
+    inquirer.prompt({
+        name: "home",
+        type: "list",
+        message: "Welcome to the employee tracker! Please select an action.",
+        choices: [
+            "Add new employee",
+            "Add new job role",
+            "Add new department",
+            "Remove employee",
+            "Remove job role",
+            "Remove department",
+            "List "
+        ]
+    })
 }
